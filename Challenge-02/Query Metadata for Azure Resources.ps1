9d3161c1b0a63f19af688f97c4431c41428f4e2e
50ur48h <130345252+50ur48h@users.noreply.github.com>
2023-07-23 17:19:43 +0530

    Add files via upload

---
+++ Challenge-02/Query Metadata for Azure Resources.ps1
@@ -0,0 +1,22 @@
+﻿# Install the Azure PowerShell module if not already installed
+Install-Module -Name Az -AllowClobber -Force
+
+
+# Import the Azure PowerShell module
+Import-Module Az
+
+# Connect to your Azure account
+Connect-AzAccount
+
+# Get a list of all resource groups
+$resourceGroups = Get-AzResourceGroup
+
+
+# Get a list of all resource groups in a Json format
+$resourceGroups = Get-AzResourceGroup | ConvertTo-Json
+
+# Get a list of all resources in a resource groups
+$resourceGroups = Get-AzResource -ResourceGroupName AiBook | ConvertTo-Json
+
+# Get a details of any perticular resource 
+$resourceGroups = Get-AzResource -Name aibook2f03fc1081 | ConvertTo-Json
